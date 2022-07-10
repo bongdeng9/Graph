1f94bdb1b0fba47f4b6c490d1d1f2a9ef9c0727e
bongdeng9 <80138709+bongdeng9@users.noreply.github.com>
2022-07-10 15:24:17 +0900

    Add files via upload

---
+++ Player.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm1
+{
+    class Pos
+    {
+        public Pos(int y, int x) { Y = y; X = x; }
+        public int Y;
+        public int X;
+    }
+    class Player
+    {
+        // 설계적 감 처음에는 누구나 없음 경험이 쌓이면 됨
+        // 플레이어의 위치를 Board.Render 에서 접근
+        public int PosY { get; private set; } // 외부에서 플레이어 위치를 get으로 질의(Read = get)는 할 수 있겠지만,  (Write = set)은 할 수 없다
+        public int PosX { get; private set; } // private set : 플레이어의 좌표정보는 자기 자신만 바꿀것
+        Random _random = new Random();
+        Board _board;
+
+        enum Dir
+        {
+            Up = 0,
+            Left = 1,
+            Down = 2,
+            Right = 3 
+        }
+
+        int _dir = (int)Dir.Up;
+        List<Pos> _points = new List<Pos>();
+
+        public void Initialize(int posY, int posX, Board board)
+        {
+            // 현재 상태르 기반으로 한칸씩  / 모든 경로를 계산해놓은다음 하나씩 업데이트에서 꺼내쓰기
+            PosY = posY;
+            PosX = posX;
+            _board = board;
+
+            BFS();
+        }
+
+        void BFS()
+        {
+            int[] deltaY = new int[] { -1, 0, 1, 0};
+            int[] deltaX = new int[] { 0, -1, 0, 1};
+
+            // 인덱스보다 좌표를 이용하는게 직관적
+            bool[,] found = new bool[_board.Size, _board.Size];
+            Pos[,] parent = new Pos[_board.Size, _board.Size];
+
+            Queue<Pos> q = new Queue<Pos>();
+            q.Enqueue(new Pos(PosY, PosX));
+            found[PosY, PosX] = true;
+            parent[PosY, PosX] = new Pos(PosY, PosX); // 시작점의 부모는 자신이다. 
+
+            while (q.Count > 0)
+            {
+                Pos pos = q.Dequeue();
+                int nowY = pos.Y;
+                int nowX = pos.X;
+
+                for (int i = 0; i < 4; i++)
+                {
+                    // 위 왼 아래 오른 순서대로 서칭 
+                    int nextY = nowY + deltaY[i];
+                    int nextX = nowX + deltaX[i];
+
+
+                    if (nextX < 0 || nextX >= _board.Size || nextY < 0 || nextY >= _board.Size)
+                        continue;
+                    if (_board.Tile[nextY, nextX] == Board.TileType.Wall) // 배열을 접근하는 코드(Tile[])는 늘 범위를 조심해야한다.
+                        continue;
+                    if (found[nextY, nextX])
+                        continue;
+
+                    q.Enqueue(new Pos(nextY, nextX)); // 예약을 다시 한다.
+                    found[nextY, nextX] = true;
+                    parent[nextY, nextX] = new Pos(nowY, nowX);
+
+                } // 이것 만으로 최단거리나 이동거리를 추출할 수 없음
+                // 여기까지 한 건 BFS 를 한번 스캔한 것 뿐
+                // 현재 관심사는 거리보다는 이동경로가 중요함 -> 부모 즉 어디서부터 왔는 지 기억하고 있으면 됨.
+            }
+            int y = _board.DestY;
+            int x = _board.DestX; // 도착한 목적지에서 거꾸로 추적해서 처음시작점까지 거슬러 올라간다.
+            while (parent[y, x].Y != y || parent[y, x].X != x) // 
+            {
+                _points.Add(new Pos(y, x));
+                Pos pos = parent[y, x];
+                y = pos.Y;
+                x = pos.X;
+            }
+            _points.Add(new Pos(y, x));  // 시작점 더해줌
+            _points.Reverse(); // 역 추적 했던 것을 뒤집어서 처음부터 찾아감
+
+        } // BFS 를 이용해서 경로를 찾음 , 우수법과는 달리 최단경로를 찾음 , BFS 의 동작방식을 생각해보면, 시작점을 기준으로 무조건 깊이 들어가는 게 아니라, 시작 위치를
+        // 기준으로 가장 가까운 노드부터 서칭을 했기 때문에 최단 경로를 이동함.
+
+        void RightHand()
+        {
+            // 현재 바라보고 있는 방향을 기준으로 좌표 변화를 나타낸다.
+            int[] frontY = new int[] { -1, 0, 1, 0 };
+            int[] frontX = new int[] { 0, -1, 0, 1 };
+            int[] rightY = new int[] { 0, -1, 0, 1 };
+            int[] rightX = new int[] { 1, 0, -1, 0 }; // 인덱스 : {Up 0 , Left 1,  Down 2,  Right 3 } 일 때 y, x 좌표 변화를 나타냄.
+
+            _points.Add(new Pos(PosY, PosX));
+            // 목적지 도착하기 전에는 계속 실행
+            while (PosY != _board.DestY || PosX != _board.DestX)
+            {
+                // 우수법 오른손 법칙 
+                // 1. 현재 바라보는 방향을 기준으로 오른쪽으로 갈 수 있는 지 확인.
+                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+                {
+                    // 오른쪽 방향으로 90도 회전
+                    _dir = (_dir - 1 + 4) % 4; // -1 빼는거는 한 칸 위로 올리는 거 +4 더한거는 양수로 취급 %4 4로 나눈 나머지
+                    // 앞으로 한 보 전진.
+                    PosY = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+
+                }
+                // 2. 현재 바라보는 방향을 기준으로 전진할 수 있는 지 확인.
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+                {
+                    // 앞으로 한 보 전진
+                    PosY = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+
+                }
+                else
+                {
+                    // 왼쪽으로 방향으로 90도 회전
+                    _dir = (_dir + 1 + 4) % 4;
+                    // 턴을 넘긴다.
+                }
+            }
+        }
+
+        const int MOVE_TICK = 10; // 0.1초
+        int _sumTick = 0;
+        int _lastIndex = 0;
+        public void Update(int deltaTick) // 30분의 1초마다 업데이트가 실행되는건 너무 빠르니까 deltaTick 을 도입해서 업데이트를 실행할 지 , 넘길 지 결정.
+        {
+            if (_lastIndex >= _points.Count)
+                return;
+
+            _sumTick += deltaTick;
+            if (_sumTick >= MOVE_TICK)
+            {
+                _sumTick = 0;
+
+                // 여기다가 0.1초마다 실행될 로직을 넣어준다.
+                PosY = _points[_lastIndex].Y;
+                PosX = _points[_lastIndex].X;
+                _lastIndex++;
+                // 배열을 사용할 때는 인덱스가 범위를 초과하지 않는 지 잘 확인해야됨.
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,102 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Excercise
+namespace Algorithm1
 {
     class Program
     {
-        class Graph
+        static void Main(string[] args)
         {
-            // 2차원 배열 or 리스트
+            Board board = new Board();
+            Player player = new Player();
+            board.Initialize(25, player);
+            player.Initialize(1, 1, board);
+            // 설계 고민 플레이어가 움직이는 것 구현 즉 길 찾는 ai 를 어디서 구현해야될까?
+            // 플레이어 좌표는 플레이어 로직 안에서 구현하는게 맞음
+            // 갈 수 있는 지 없는 지 판단하기 위해서는 board 에 대한 정보가 필요함.
 
-            // 행렬버전
-            int[,] adj = new int[6, 6]
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+
+            int lastTick = 0;
+            while (true)
             {
-                { 0, 1, 0, 1, 0, 0 },
-                { 1, 0, 1, 1, 0, 0 },
-                { 0, 1, 0, 0, 0, 0 },
-                { 1, 1, 0, 0, 0, 0 },
-                { 0, 0, 0, 0, 0, 1 },
-                { 0, 0, 0, 0, 1, 0 },
-            };
+                #region 프레임관리
+                // FPS 프레임 (60 프레임) 이 루프가 1초에 몇번 업데이트 하는지 
+                int currentTick = System.Environment.TickCount;
 
-            // 리스트버전
-            List<int>[] adj2 = new List<int>[]
-            {
-                new List<int>() { 1, 3},
-                new List<int>() { 0, 2, 3},
-                new List<int>() { 1},
-                new List<int>() { 0, 1},
-                new List<int>() { 5},
-                new List<int>() { 4},
-            };
+                // 만약에 경과한 시간이 1/30초 보다 작다면,
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+                #endregion
 
-            // 1) 우선 now 부터 방문하고,
-            // 2) now 와 연결된 정점들을 하나씩 확인해서, [아직 미방문 상태라면] 방문한다.
-            bool[] visited = new bool[6]; // DFS 함수들이 어디서든 접근할 수 있도록 공용 공간을 만듦. DFS() 인자 파라미터로 넘기기 귀찮으면 이렇게..
-            public void DFS(int now)
-            {
-                Console.WriteLine(now);
-                visited[now] = true;
+                // 입력
 
-                for (int next = 0; next < adj.GetLength(0); next++)
-                {
-                    if (adj[now, next] == 0) // 연결 되어 있지 않으면 스킵
-                        continue;
-                    if (visited[next]) // 이미 방문했으면 스킵
-                        continue;
-                    DFS(next);
-                    // now 클리어 표시 visited
-                    // next 다음 정점을 기준으로 같은 행동을 반복한다.
-                    // 오케이 찍고 타고 다음거로 간다. 이 행동이 반복됨.
-                    // 재귀함수 사용
+                // 로직 데이터가 변하는 부분
+                player.Update(deltaTick); // 1000 / 30 초마다 업데이트가 실행됨.
 
-                }
-            }
-            public void DFS2(int now)
-            {
-                Console.WriteLine(now);
-                visited[now] = true;
-
-                foreach(int next in adj2[now]) // 애초에 연결된 놈들만 배열들이 리스트에 담겨있음 DFS 에서랑은 다르게 DFS2 는 연결되어 있는지 체크 안해도 됨.
-                {
-                    if (visited[next]) // 이미 방문했으면 스킵
-                        continue;
-                    DFS2(next);
-                }
+                // 렌더링 그림만 그리는 부분
+                Console.SetCursorPosition(0, 0);
+                board.Render();
             }
 
-            public void SearchAll() // edge가 끊겨있어도 모든 노드 서치하는 함수
-            {
-                visited = new bool[6];
-                for (int now = 0; now < 6; now++)
-                    if (visited[now] == false)
-                        DFS(now); // DFS를 한번 실행 하는 순간, 끊기기전 부분은 모두 TRUE로 바뀜
-            }
         }
-
-            static void Main(string[] args)
-            {
-                // 그래프의 기준? 
-                // 그래프를 순회하는 방법 1. DFS(깊이 우선 탐색) 
-                //                        2. BFS(너비 우선 탐색)
-
-                // 던전을 생각한다.
-
-                // DFS 는 무모한 용사 보이는 대로 무조건 들어감.
-                // 최종보스까지 달려감. 
-                // 더이상 갈 곳이 없으면 되돌아감.
-                // 왔던 길을 계속 돌아가다가 안 가봤던 길을 발견하면 거기로 간다.
-                // 똑같은 로직으로 안 가본 곳까지 간다.
-
-                 Graph graph = new Graph();
-                 graph.SearchAll();
-
-
-
-            }
-
-
-        
     }
 }
